149736f54e509b45ad5f4a59bc04ae820aeea633
jessi994 <jessied200@yahoo.co.uk>
2024-02-15 15:59:52 +0000

    Colour changing on time for blocks fixed

--- starter/script.js
+++ starter/script.js
@@ -4,44 +4,29 @@ var rightNow;
 // Get the current time and display 
 $(document).ready(function () {
     displayTime();
-    loadData() 
+    loadData();
+    updateTimeBlocks();
 });
 
 // Get current date
 function displayTime() {
-   
+
     rightNow = dayjs().format('DD MMM YYYY');
     timeDisplayEl.text(rightNow);
 
     // If currentDate is not set, use today's date
-    if(!localStorage.getItem('currentDate')) {
+    if (!localStorage.getItem('currentDate')) {
         localStorage.setItem('currentDate', rightNow);
     }
 
 }
 
-// Assign colour to block depending on whether time associated with block is in the past, present or future
-document.addEventListener('DOMContentLoaded', function () {
-    const currentTime = new Date().getHours();
-    const timeBlocks = document.querySelectorAll('.time-block');
 
-    timeBlocks.forEach(block => {
-        const blockHour = parseInt(block.getAttribute('data-hour'), 10);
-
-        if (blockHour < currentTime) {
-            block.classList.add('past');
-        } else if (blockHour === currentTime) {
-            block.classList.add('present');
-        } else {
-            block.classList.add('future');
-        }
-    });
-});
 
 //  Save user input into local storage when 'save' button clicked
 $(document).ready(function () {
     $('.container').on('click', '.saveBtn', function () {
-        var hourTime = $(this).closest('.time-block').find('.hour').text(); // Make sure this matches your data attribute
+        var hourTime = $(this).closest('.time-block').find('.hour').text();
         console.log('Hour:', hourTime);
         var userText = $(this).closest('.time-block').find('.description').val();
         console.log("User Text:", userText);
@@ -52,13 +37,13 @@ $(document).ready(function () {
 // Saving data from time blocks into local storage
 function savingData(hourTime, userText) {
     localStorage.setItem('work-block-' + hourTime, userText);
-    console.log('Data saved for hour ' + hourTime + ': ' + userText); 
+    console.log('Data saved for hour ' + hourTime + ': ' + userText);
 }
 
 // If currentDate doesn't match rightNow, clear local storage
 function loadData() {
-    console.log(typeof localStorage.getItem('currentDate'))
-    if(localStorage.getItem('currentDate') !== rightNow){
+    // console.log(typeof localStorage.getItem('currentDate'))
+    if (localStorage.getItem('currentDate') !== rightNow) {
         localStorage.clear();
         localStorage.setItem('currentDate', rightNow);
 
@@ -75,4 +60,35 @@ function loadData() {
     }
 }
 
+// Assign colour to block depending on whether time associated with block is in the past, present or future
+function updateTimeBlocks() {
+    const currentTime = dayjs();
+    const currentHour24 = currentTime.hour();
+
+    $('.time-block').each(function () {
+        const block = $(this);
+        let blockTimeText = block.find('.hour').text();
+        // Get hour number
+        let blockHour = parseInt(blockTimeText, 10);
+        // Check if time ends with "PM"
+        let isBlockPM = /PM$/.test(blockTimeText);
+
+        // Convert block hour to 24-hour format for comparison
+        if (isBlockPM && blockHour !== 12) {
+            blockHour += 12;
+        }
+
+        // Remove existing color classes
+        block.removeClass('past present future');
+
+        // Assign new color based on comparison
+        if (blockHour < currentHour24) {
+            block.addClass('past');
+        } else if (blockHour === currentHour24) {
+            block.addClass('present');
+        } else {
+            block.addClass('future');
+        }
+    });
+}
 
